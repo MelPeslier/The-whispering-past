8865be2a8f04fb1551c95a482887c8ac86e1d04a
MelPeslier <mel.peslier@gmail.com>
2023-05-21 02:22:10 -0300

    ajout bar sun

--- UI/moon_bar.gd
+++ UI/moon_bar.gd
@@ -4,23 +4,26 @@ extends TextureProgressBar
 
 const U: float = 0.64
 
+var t: float = 0.22
+
 func _ready() -> void:
 	Events.connect("add_moon", _on_add_moon)
-	value = 100
+	value = 0
 	update_style_moon(value as float)
 
 func _on_add_moon(val: int):
-	value = val
-	update_style_moon(value as float)
-	if value > 0:
-#		update_style_diams()
-		pass
+	update_style_moon(val as float)
+	#update_style_diams()
 
 func update_style_moon(val: float):
-	# Partie shader pourla couleur de la progress bar
-	set("material:shader_parameter/val", val)
+	var tween: Tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT).set_parallel()
+	tween.tween_property(self, "value", val as int, t)
+	tween.tween_property(self, "material:shader_parameter/val", val, t)
 	
-	# Partie Particules
-	moon_particles.process_material.set("emission_box_extents", Vector3(val * U, 10.0, 1.0))
-	moon_particles.set("position", Vector2(128.0 - val * U, 16.0))
-	moon_particles.set("amount", val)
+	tween.tween_property(moon_particles, "process_material:emission_box_extents", Vector3(val * U, 10.0, 1.0), t)
+	tween.tween_property(moon_particles, "position", Vector2(128.0 - val * U, 16.0), t)
+	moon_particles.set("amount", clamp(val, 1, 100))
+#	set("material:shader_parameter/val", val)
+#	moon_particles.process_material.set("emission_box_extents", Vector3(val * U, 10.0, 1.0))
+#	moon_particles.set("position", Vector2(128.0 - val * U, 16.0))
+#	moon_particles.set("amount", clamp(val, 1, 100))
--- UI/sun_bar.gd
+++ UI/sun_bar.gd
@@ -1,7 +1,28 @@
 extends TextureProgressBar
 
+@onready var sun_particles: GPUParticles2D = $SunParticles
+
+const U: float = 0.64
+var t: float = 0.3
+
 func _ready() -> void:
 	Events.connect("add_sun", _on_add_sun)
+	value = 0
+	update_style_sun(value as float)
 
 func _on_add_sun(val: int):
-	value = val
+	update_style_sun(100.0)
+	#update_style_diams()
+
+func update_style_sun(val: float):
+	var tween: Tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT).set_parallel()
+	tween.tween_property(self, "value", val as int, t)
+	tween.tween_property(self, "material:shader_parameter/val", val, t)
+	
+	tween.tween_property(sun_particles, "process_material:emission_box_extents", Vector3(val * U, 10.0, 1.0), t)
+	tween.tween_property(sun_particles, "position", Vector2(128.0 - val * U, 16.0), t)
+	sun_particles.set("amount", clamp(val, 1, 100))
+	
+	#set("material:shader_parameter/val", val)
+#	sun_particles.process_material.set("emission_box_extents", Vector3(val * U, 10.0, 1.0))
+#	sun_particles.set("position", Vector2(128.0 - val * U, 16.0))
